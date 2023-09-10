8ebedb375e267b85bd1a28e24fb4399344dc5e0f
Simone Saleri <132743672+simonesalerisem@users.noreply.github.com>
2023-09-10 11:28:23 +0200

    created car models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,22 +4,40 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+from django.db import models
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    # You can add any other fields you would like to include in car make model here
+    # ...
 
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    SEDAN = 'SD'
+    SUV = 'SV'
+    WAGON = 'WG'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        # Add more types if needed
+    ]
+
+    car_make = models.ForeignKey(CarMake, related_name="car_models", on_delete=models.CASCADE)
+    dealer_id = models.IntegerField() # assuming dealer_id is an integer
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    year = models.DateField()
+
+    # You can add any other fields you would like to include in car model here
+    # ...
+
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
